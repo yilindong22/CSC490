d5a4d1b7f439008e1da2c2a27eddafaf335986a6
yilindong22 <yilindong22@gmail.com>
2023-05-22 20:37:11 -0700

    completed code
    test.c is for the random read access, and test2.c is for the Sequential read access

--- test2.c
+++ test2.c
@@ -11,34 +11,23 @@ typedef struct l {
 }l;
 
 int main() {
-    
-
     int n = pow(2,19) / sizeof(struct l);
 
 
-    l** rarr = malloc(n * sizeof(l*)); //random array
+    l** sarr = malloc(n * sizeof(l*)); //sequential array
 
     struct l* head = NULL;
     head = malloc(sizeof(struct l));  //1
     l* node = head;  //head -> next 
 
     for (int i = 0; i < n-1; i++){  //creat more nodes and insert them into the array
-        rarr[i] = node;
+        sarr[i] = node;
         node -> n = malloc(sizeof(struct l)); 
         node = node->n;
         node -> n = NULL;
     }
-    struct l* curr = head;
-
-    for(int i = 0; i < n; i++) { // shuffle the random array 
-        int index1 = rand() % 10;
-        int index2 = rand() % 10;
-        l* temp = rarr[index1];
-        rarr[index1] = rarr[index2];
-        rarr[index2] = temp;
-    }
     double time_spent = 0.0;
-
+ 
     clock_t begin = clock();
     while(head->n != NULL){
         head = head->n;
@@ -49,7 +38,6 @@ int main() {
     // dividing the difference by CLOCKS_PER_SEC to convert to seconds
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
  
-    printf("The ran elapsed time is %f seconds", time_spent);
-
+    printf("The seq elapsed time is %f seconds", time_spent);
     return 0;
 }
